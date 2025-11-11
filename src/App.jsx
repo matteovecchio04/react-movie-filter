9dfdcd0c5fa661f15bfc3daa357852bb24416f52
matteovecchio04 <matteovex@gmail.com>
2025-11-11 12:56:41 +0100

    add icons to genre for better looks

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 const movies = [
-  { title: 'Inception', genre: 'Fantascienza' },
-  { title: 'Il Padrino', genre: 'Thriller' },
-  { title: 'Titanic', genre: 'Romantico' },
-  { title: 'Batman', genre: 'Azione' },
-  { title: 'Interstellar', genre: 'Fantascienza' },
-  { title: 'Pulp Fiction', genre: 'Thriller' },
+  { title: 'Inception', genre: 'Fantascienza 🛸' },
+  { title: 'Il Padrino', genre: 'Thriller 🔪' },
+  { title: 'Titanic', genre: 'Romantico ❤️' },
+  { title: 'Batman', genre: 'Azione 🚓' },
+  { title: 'Interstellar', genre: 'Fantascienza 🛸' },
+  { title: 'Pulp Fiction', genre: 'Thriller 🔪' },
 ]
 
 export default function App() {
@@ -33,16 +33,16 @@ export default function App() {
         <strong className='mx-2'>Seleziona il genere:</strong>
         <select value={selectGenre} onChange={(e) => setselectGenre(e.target.value)}>
           <option value="">Tutti i generi</option>
-          <option value="Fantascienza">Fantascienza</option>
-          <option value="Thriller">Thriller</option>
-          <option value="Romantico">Romantico</option>
-          <option value="Azione">Azione</option>
+          <option value="Fantascienza 🛸">Fantascienza</option>
+          <option value="Thriller 🔪">Thriller</option>
+          <option value="Romantico ❤️">Romantico</option>
+          <option value="Azione 🚓">Azione</option>
         </select>
 
         <ul className='list-unstyled'>
           {
             filterMovies.map((film) => (
-              <li key={film.title}>
+              <li key={film.title} className=''>
                 {film.title} - {film.genre}
               </li>
             ))
